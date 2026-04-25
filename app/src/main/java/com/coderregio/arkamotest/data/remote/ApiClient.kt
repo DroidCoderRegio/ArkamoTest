a8deaebf835566b3523bdb136727e9d4ef58afa9
DroidCoderRegio <droidcoderregio@gmail.com>
2026-04-25 12:19:58 -0600

    feat: Configurar cliente Ktor

---
+++ app/src/main/java/com/coderregio/arkamotest/data/remote/ApiClient.kt
@@ -0,0 +1,24 @@
+package com.coderregio.arkamotest.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object ApiClient {
+
+    private const val BASE_URL = "https://rickandmortyapi.com/api/character"
+
+    val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+
+    fun getBaseUrl(): String = BASE_URL
+}
